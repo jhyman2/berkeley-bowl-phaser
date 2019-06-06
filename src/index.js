095fb18420cc74b2a60d586479f77dfca8fa2150
Jamison Hyman <jamison.hyman@gmail.com>
2019-06-05 23:19:38 -0700

    Get cart movement working

--- src/index.js
+++ src/index.js
@@ -1,32 +1,81 @@
 import Phaser from "phaser";
 import logoImg from "./assets/logo.jpg";
+import shoppingCartImg from './assets/shopping_cart.png';
+import blockImg from './assets/block.jpeg';
+import steelboxImg from './assets/steelbox.png';
+import bgImg from './assets/background.png';
 
 const config = {
   type: Phaser.AUTO,
-  parent: "phaser-example",
-  width: 800,
-  height: 600,
-  scene: {
-    preload: preload,
-    create: create
-  }
+    width: 800,
+    height: 600,
+    parent: 'phaser-example',
+    physics: {
+        default: 'arcade',
+        arcade: {
+            debug: true
+        }
+    },
+    scene: {
+        preload,
+        create,
+        update
+    }
 };
 
-const game = new Phaser.Game(config);
+let player;
+let cursors;
 
-function preload() {
-  this.load.image("logo", logoImg);
+let game = new Phaser.Game(config);
+
+function preload ()
+{
+  this.load.image('block', blockImg);
+  this.load.image('shoppingCart', shoppingCartImg);
+  this.load.image('bg', bgImg);
 }
 
-function create() {
-  const logo = this.add.image(400, 150, "logo");
+function create ()
+{ 
+     // todo: make these lines show up
+    var line1 = new Phaser.Curves.Line([ 0, 100, 200, 200, '0xff0000' ]);
+    var line2 = new Phaser.Curves.Line([ 0, 200, 300, 300, '0xff0000' ]);
+    this.add.image(0, 0, 'bg').setOrigin(0);
 
-  this.tweens.add({
-    targets: logo,
-    y: 450,
-    duration: 2000,
-    ease: "Power2",
-    yoyo: true,
-    loop: -1
-  });
+    var block = this.physics.add.image(400, 300, 'block').setImmovable(true).setName('big');
+
+    //  Allow entrance through the top-face only
+    // block.body.setCheckCollisionUp(false);
+
+    player = this.physics.add.image(100, 300, 'shoppingCart').setName('small');
+
+    // this.physics.add.collider(block, player);
+
+    this.physics.add.collider(player, block);
+
+    cursors = this.input.keyboard.createCursorKeys();
+
+}
+
+function update (time)
+{
+    player.setVelocity(0);
+
+    if (cursors.left.isDown)
+    {
+        player.setVelocityX(-200);
+    }
+    else if (cursors.right.isDown)
+    {
+        player.setVelocityX(200);
+    }
+
+    if (cursors.up.isDown)
+    {
+        player.setVelocityY(-200);
+    }
+    else if (cursors.down.isDown)
+    {
+        player.setVelocityY(200);
+    }
 }
